c854def722fd47d8584968f83609f949526a630d
Ken Chang <kenchang8787@gmail.com>
2021-08-07 10:43:01 +0800

    base - For 迴圈

---
+++ base/2_If/if.go
@@ -0,0 +1,28 @@
+/*
+If
+If else
+If elseif
+	這三個都跟認知上的用法差不多, 就不多贅述
+*/
+package main
+
+func main() {
+	ifExample()
+	ifElseExample()
+	ifElseIfExample()
+}
+
+// If 範例
+func ifExample() {
+
+}
+
+// If Else 範例
+func ifElseExample() {
+
+}
+
+// If ElseIf 範例
+func ifElseIfExample() {
+
+}
